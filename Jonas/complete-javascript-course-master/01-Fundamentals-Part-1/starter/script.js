d50388063723aeea905ab96f4574d00c94c6b616
Vidit Shah <ml02100@yahoo.com>
2021-01-18 11:44:32 +0530

    PrimitiveDataTypes

--- Jonas/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ Jonas/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,18 @@
 let js = 'amazing';
 
-if (js === 'amazing') {
-	alert('Js is fun');
-}
+console.log('Jonas');
+console.log(23);
+let firstName = 'Jonas';
+console.log(firstName);
+
+let myFirstJob = 'Programmer';
+let mySecondJob = 'Senior Software Engineer';
+/**
+  * Primititive Data Types
+  * Numbers
+  * String
+  * Boolean
+  * Undefined
+  * Null
+  * BigInt
+  * */
