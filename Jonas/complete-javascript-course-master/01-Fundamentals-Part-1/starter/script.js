e4b0d6d31b2c3d87540b55011945db0b07ffc439
Vidit Shah <ml02100@yahoo.com>
2021-01-18 18:21:15 +0530

    p1

--- Jonas/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ Jonas/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -19,5 +19,13 @@ let mySecondJob = 'Senior Software Engineer';
 
 let allowPassenger = true;
 let javascriptIsFun = true;
-
+console.log(typeof javascriptIsFun);
 console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof myFirstJob);
+
+javascriptIsFun = 'YES';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(typeof year);
