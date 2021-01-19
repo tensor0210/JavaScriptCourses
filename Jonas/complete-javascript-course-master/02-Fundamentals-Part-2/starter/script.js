602a9ef5f791e0f3e88e8121a7d24710ca9913c5
Vidit Shah <ml02100@yahoo.com>
2021-01-19 10:22:32 +0530

    expressions

--- Jonas/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
+++ Jonas/complete-javascript-course-master/02-Fundamentals-Part-2/starter/script.js
@@ -1,15 +1,30 @@
 'use strict';
 
-function logger() {
-	console.log('My name is Vidit');
-}
-logger();
+// function logger() {
+// 	console.log('My name is Vidit');
+// }
+// logger();
 
-function fruitProcessor(apples, orange) {
-	console.log(apples, orange);
-	const juice = `Juice with ${apples} apple and ${orange}`;
-	return juice;
-}
+// function fruitProcessor(apples, orange) {
+// 	console.log(apples, orange);
+// 	const juice = `Juice with ${apples} apple and ${orange}`;
+// 	return juice;
+// }
 
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+
+// function calcAge(birthYear) {
+// 	const age = 2021 - birthYear;
+// 	return age;
+// }
+// console.log(calcAge(1996));
+
+// const calcAge = function(birthYear) {
+// 	return 2021 - birthYear;
+// };
+
+// const age2 = calcAge(1991);
+// console.log(typeof calcAge);
+
