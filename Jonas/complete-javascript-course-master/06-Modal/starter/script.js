acd5a5f5113f89c19777ac39e1d2f013fc7b6433
Vidit Shah <ml02100@yahoo.com>
2021-01-21 16:17:16 +0530

    hidden

--- Jonas/complete-javascript-course-master/06-Modal/starter/script.js
+++ Jonas/complete-javascript-course-master/06-Modal/starter/script.js
@@ -9,5 +9,6 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 	btnsOpenModal[i].addEventListener('click', function() {
 		console.log('Button Clicked');
 		modal.classList.remove('hidden');
+		overlay.classList.remove('hidden');
 	});
 }
