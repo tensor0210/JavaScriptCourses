f58aa218dada0a77d73e33bb59ba84150b7c1049
Vidit Shah <ml02100@yahoo.com>
2021-01-21 22:28:09 +0530

    Update script.js

--- Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -14,18 +14,34 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 //Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+let scores = undefined;
 
-const scores = [
-	0,
-	0
-];
+let currentScore;
+let activePlayer;
+let playing;
 
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function() {
+	scores = [
+		0,
+		0
+	];
+
+	currentScore = 0;
+	activePlayer = 0;
+	playing = true;
+	score0El.textContent = 0;
+	score1El.textContent = 0;
+	current0El.textContent = 0;
+	current0El.textContent = 0;
+	diceEl.classList.add('hidden');
+	player0El.classList.remove('player--winner');
+	player1El.classList.remove('player--winner');
+	player0El.classList.add('player--active');
+	player1El.classList.remove('player--active');
+};
+
+init();
+
 const switchPlayer = function() {
 	document.getElementById(`current--${activePlayer}`).textContent = 0;
 	currentScore = 0;
@@ -53,7 +69,7 @@ btnHold.addEventListener('click', function() {
 		scores[activePlayer] += currentScore;
 
 		document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
-		if (scores[activePlayer] >= 10) {
+		if (scores[activePlayer] >= 100) {
 			playing = false;
 			document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
 			document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
@@ -74,3 +90,5 @@ btnHold.addEventListener('click', function() {
 		}
 	}
 });
+
+btnNew.addEventListener('click', init);
