1f0a160cf20ad9d72993fb0e6cdf68dac5a40831
Vidit Shah <ml02100@yahoo.com>
2021-01-21 16:15:36 +0530

    modal

--- Jonas/complete-javascript-course-master/06-Modal/starter/script.js
+++ Jonas/complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,13 @@
 'use strict';
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+	btnsOpenModal[i].addEventListener('click', function() {
+		console.log('Button Clicked');
+		modal.classList.remove('hidden');
+	});
+}
