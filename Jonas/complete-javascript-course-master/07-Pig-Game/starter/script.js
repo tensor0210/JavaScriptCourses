fef8fb947741a9c11c4fb41e390bba45a144433e
Vidit Shah <ml02100@yahoo.com>
2021-01-21 18:26:52 +0530

    currentScore

--- Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -4,6 +4,8 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
@@ -13,8 +15,15 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 btnRoll.addEventListener('click', function() {
 	const dice = Math.trunc(Math.random() * 6) + 1;
 	diceEl.classList.remove('hidden');
 	diceEl.src = `dice-${dice}.png`;
+	if (dice !== 1) {
+		currentScore = currentScore + dice;
+		current0El.textContent = currentScore;
+	} else {
+	}
 });
