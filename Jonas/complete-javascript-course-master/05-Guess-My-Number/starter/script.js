f3dd09380412cf972e1637132c47075da4d63b69
Vidit Shah <ml02100@yahoo.com>
2021-01-21 10:50:54 +0530

    message

--- Jonas/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ Jonas/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -14,5 +14,9 @@
 // console.log(document.querySelector('.guess').value);
 
 document.querySelector('.check').addEventListener('click', function() {
-	console.log(document.querySelector('.guess').value);
+	const guess = Number(document.querySelector('.guess').value);
+	console.log(guess);
+	if (!guess) {
+		document.querySelector('.message').textContent = 'No Number';
+	}
 });
