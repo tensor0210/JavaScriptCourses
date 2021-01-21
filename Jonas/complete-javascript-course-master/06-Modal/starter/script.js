40a5b3b4e1fcafaa8433647e8e54c9f6c46fa8af
Vidit Shah <ml02100@yahoo.com>
2021-01-21 16:34:51 +0530

    modal subscribtion

--- Jonas/complete-javascript-course-master/06-Modal/starter/script.js
+++ Jonas/complete-javascript-course-master/06-Modal/starter/script.js
@@ -3,17 +3,21 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnsOpenModal);
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-	btnsOpenModal[i].addEventListener('click', function() {
-		console.log('Button Clicked');
-		modal.classList.remove('hidden');
-		overlay.classList.remove('hidden');
-	});
-}
+const openModal = function() {
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+};
 
-btnCloseModal.addEventListener('click', function() {
+const closedModal = function() {
 	modal.classList.add('hidden');
 	overlay.classList.add('hidden');
-});
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+	btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closedModal);
+
+overlay.addEventListener('click', closedModal);
