ef443e53760fffccfd62685b43093e9434ceac3c
Vidit Shah <ml02100@yahoo.com>
2021-01-21 13:40:18 +0530

    validator2

---
+++ formValidator/script.js
@@ -0,0 +1,43 @@
+'use strict';
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('password2');
+
+function showError(input, message) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control error';
+	const small = formControl.querySelector('small');
+	small.innerText = message;
+}
+
+function showSuccess(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+
+function isValidEmail(email) {
+	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
+
+function checkRequired(inputArr) {
+	inputArr.forEach(function(input) {
+		if (input.value.trim() === '') {
+			showError(input, 'is Required');
+		} else {
+			showSuccess(input);
+		}
+	});
+}
+
+form.addEventListener('submit', function(e) {
+	e.preventDefault();
+	checkRequired([
+		username,
+		email,
+		password,
+		confirmPassword
+	]);
+});
