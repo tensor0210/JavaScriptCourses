db5cc183c4e1894fb4bb92d1bc9019f9526bda6e
Vidit Shah <ml02100@yahoo.com>
2021-01-21 18:22:32 +0530

    roll

--- Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ Jonas/complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -4,8 +4,17 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //Starting Conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+btnRoll.addEventListener('click', function() {
+	const dice = Math.trunc(Math.random() * 6) + 1;
+	diceEl.classList.remove('hidden');
+	diceEl.src = `dice-${dice}.png`;
+});
