18e2520a73d84ca9f886cd32279e08ff5aa10e74
Vidit Shah <ml02100@yahoo.com>
2021-01-21 10:59:26 +0530

    score

--- Jonas/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ Jonas/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -13,10 +13,23 @@
 // document.querySelector('.guess').value = 100;
 // console.log(document.querySelector('.guess').value);
 
+const NUMBER = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+console.log(NUMBER);
 document.querySelector('.check').addEventListener('click', function() {
 	const guess = Number(document.querySelector('.guess').value);
 	console.log(guess);
 	if (!guess) {
 		document.querySelector('.message').textContent = 'No Number';
+	} else if (guess === NUMBER) {
+		document.querySelector('.message').textContent = 'Correct 🎉';
+	} else if (guess > NUMBER) {
+		document.querySelector('.message').textContent = 'Too high';
+		score--;
+		document.querySelector('.score').textContent = score;
+	} else {
+		document.querySelector('.message').textContent = 'Too low';
+		score--;
+		document.querySelector('.score').textContent = score;
 	}
 });
