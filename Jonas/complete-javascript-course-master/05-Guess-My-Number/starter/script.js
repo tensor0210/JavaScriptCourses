8bb4c7e756313212e9228786756ef2177928abb3
Vidit Shah <ml02100@yahoo.com>
2021-01-21 10:33:05 +0530

    dom

--- Jonas/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
+++ Jonas/complete-javascript-course-master/05-Guess-My-Number/starter/script.js
@@ -1,3 +1,15 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = 'Correct Number 🎉';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// // For input we use value
+// document.querySelector('.guess').value = 100;
+// console.log(document.querySelector('.guess').value);
+
