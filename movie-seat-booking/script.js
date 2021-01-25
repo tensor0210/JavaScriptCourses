a96cc62fe488e9e6bb13e56d6564306033dfacda
Vidit Shah <ml02100@yahoo.com>
2021-01-25 10:28:39 +0530

    script

---
+++ movie-seat-booking/script.js
@@ -0,0 +1,63 @@
+'use strict';
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row.seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+populateUI();
+
+let ticketPrie = +movieSelect.value;
+
+//Save Selected movie Index and price;
+function setMovieData(movieIndex, moviePrice) {
+	localStorage.setItem('selectedMovieIndex', movieIndex);
+	localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+//update total and count
+function updateSelectedCount() {
+	const selectedSeats = document.querySelectorAll('.row.seat.selected');
+
+	const seatIndex = [
+		...selectedSeats
+	].map((seat) =>
+		[
+			...seats
+		].indexOf(seat)
+	);
+
+	localStorage.setItem('selectedSeats', JSON.stringify(seatIndex));
+	const selectedSeatsCount = selectedSeats.length;
+
+	count.innerText = selectedSeatsCount;
+	total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+function populateUI() {
+	const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+	if (selectedSeats !== null && selectedSeats.length > 0) {
+		seats.forEach((seat, index) => {
+			if (selectedSeats.indexOf(index) > -1) {
+				seat.classList.add('selected');
+			}
+		});
+	}
+	if (selectedMovieIndex !== null) {
+		movieSelect.selectedIndex = selectedMovieIndex;
+	}
+}
+
+movieSelect.addEventListener('change', (e) => {
+	ticketPrice = +e.target.value;
+	setMovieData(e.target.selectedIndex, e.target.value);
+	updateSelectCount();
+});
+
+container.addEventListener('click', (e) => {
+	if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
+		e.target.classList.toggle('selected');
+		updateSelectCount;
+	}
+});
