247bdda5f4115f6d385a154e314b687ae274cde2
Vidit Shah <ml02100@yahoo.com>
2021-01-26 10:52:43 +0530

    f1

--- Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -99,39 +99,17 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
-// LECTURES
+const displayMovements = function(movements) {
+	containerMovements.innerHTML = movements;
+	movements.forEach((mov, i) => {
+		const type = mov > 0 ? 'deposit' : 'withdrawal';
+		const html = ` <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i + 1} -${type}</div>
+          <div class="movements__date">3 days ago</div>
+          <div class="movements__value">${mov}€</div>
+        </div>`;
+		containerMovements.insertAdjacentHTML('afterbegin', html);
+	});
+};
 
-const movements = [
-	200,
-	450,
-	-400,
-	3000,
-	-650,
-	-130,
-	70,
-	1300
-];
-
-/////////////////////////////////////////////////
-
-//For each with Maps and set
-const currencies = new Map([
-	[
-		'USD',
-		'United States dollar'
-	],
-	[
-		'EUR',
-		'Euro'
-	],
-	[
-		'GBP',
-		'Pound sterling'
-	]
-]);
-
-currencies.forEach((key, value) => {
-	console.log(`${key} ${value}`);
-});
+displayMovements(account1.movements);
