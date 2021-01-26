4f2610f9c83769981a7105fce0ff2135d25f6bc1
Vidit Shah <ml02100@yahoo.com>
2021-01-26 10:17:49 +0530

    c11

--- Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ Jonas/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -6,34 +6,72 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+	owner        : 'Jonas Schmedtmann',
+	movements    : [
+		200,
+		450,
+		-400,
+		3000,
+		-650,
+		-130,
+		70,
+		1300
+	],
+	interestRate : 1.2, // %
+	pin          : 1111
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+	owner        : 'Jessica Davis',
+	movements    : [
+		5000,
+		3400,
+		-150,
+		-790,
+		-3210,
+		-1000,
+		8500,
+		-30
+	],
+	interestRate : 1.5,
+	pin          : 2222
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
+	owner        : 'Steven Thomas Williams',
+	movements    : [
+		200,
+		-200,
+		340,
+		-300,
+		-20,
+		50,
+		400,
+		-460
+	],
+	interestRate : 0.7,
+	pin          : 3333
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
+	owner        : 'Sarah Smith',
+	movements    : [
+		430,
+		1000,
+		700,
+		50,
+		90
+	],
+	interestRate : 1,
+	pin          : 4444
 };
 
-const accounts = [account1, account2, account3, account4];
+const accounts = [
+	account1,
+	account2,
+	account3,
+	account4
+];
 
 // Elements
 const labelWelcome = document.querySelector('.welcome');
@@ -66,11 +104,46 @@ const inputClosePin = document.querySelector('.form__input--pin');
 // LECTURES
 
 const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
+	[
+		'USD',
+		'United States dollar'
+	],
+	[
+		'EUR',
+		'Euro'
+	],
+	[
+		'GBP',
+		'Pound sterling'
+	]
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [
+	200,
+	450,
+	-400,
+	3000,
+	-650,
+	-130,
+	70,
+	1300
+];
 
 /////////////////////////////////////////////////
+
+let arr = [
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f'
+];
+
+console.log(arr.splice(1, 2, 6, 6, 6, 6));
+console.log(arr);
+console.log(arr.reverse());
+console.log(arr);
+
+const letters = arr.concat(arr);
+console.log(letters);
