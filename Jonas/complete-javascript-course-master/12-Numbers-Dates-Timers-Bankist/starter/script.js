0f8286597831eb6253cc0c89ef792145f0e4708d
Vidit Shah <ml02100@yahoo.com>
2021-01-27 15:29:50 +0530

    dates

--- Jonas/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ Jonas/complete-javascript-course-master/12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -113,7 +113,7 @@ const displayMovements = function(movements, sort = false) {
 		const html = `
       <div class="movements__row">
         <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -123,15 +123,15 @@ const displayMovements = function(movements, sort = false) {
 
 const calcDisplayBalance = function(acc) {
 	acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-	labelBalance.textContent = `${acc.balance}€`;
+	labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function(acc) {
 	const incomes = acc.movements.filter((mov) => mov > 0).reduce((acc, mov) => acc + mov, 0);
-	labelSumIn.textContent = `${incomes}€`;
+	labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
 	const out = acc.movements.filter((mov) => mov < 0).reduce((acc, mov) => acc + mov, 0);
-	labelSumOut.textContent = `${Math.abs(out)}€`;
+	labelSumOut.textContent = `${Math.abs(out).toFixed(2)}€`;
 
 	const interest = acc.movements
 		.filter((mov) => mov > 0)
@@ -141,7 +141,7 @@ const calcDisplaySummary = function(acc) {
 			return int >= 1;
 		})
 		.reduce((acc, int) => acc + int, 0);
-	labelSumInterest.textContent = `${interest}€`;
+	labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function(accs) {
@@ -211,7 +211,7 @@ btnTransfer.addEventListener('click', function(e) {
 btnLoan.addEventListener('click', function(e) {
 	e.preventDefault();
 
-	const amount = Number(inputLoanAmount.value);
+	const amount = Math.floor(inputLoanAmount.value);
 
 	if (amount > 0 && currentAccount.movements.some((mov) => mov >= amount * 0.1)) {
 		// Add movement
@@ -251,4 +251,3 @@ btnSort.addEventListener('click', function(e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
-const randomint = (min, max) => Math.trunc(Math.floor(Math.random() * (max - min) + 1));
